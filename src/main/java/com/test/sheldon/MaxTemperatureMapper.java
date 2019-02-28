527fef99d1bfd1e01b52858e7efa8cf7a78d33ee
sheldon <jinrun.xie@qq.com>
2019-02-28 02:15:08 -0800

    commit

--- src/main/java/com/test/sheldon/MaxTemperatureMapper.java
+++ src/main/java/com/test/sheldon/MaxTemperatureMapper.java
@@ -7,21 +7,31 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
 
-public class MaxTemperatureMapper extends Mapper<LongWritable, Text,Text, IntWritable> {
+public class MaxTemperatureMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
     private static final int MISSING = 9999;
+
+    enum Temperature {
+        OVER_100
+    }
+
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String line = value.toString();
-        String year = line.substring(15,19);
+        String year = line.substring(15, 19);
         int airTemperature;
-        if(line.charAt(87) == '+'){
-            airTemperature = Integer.parseInt(line.substring(88,92));
-        }else{
-            airTemperature = Integer.parseInt(line.substring(87,92));
+        if (line.charAt(87) == '+') {
+            airTemperature = Integer.parseInt(line.substring(88, 92));
+        } else {
+            airTemperature = Integer.parseInt(line.substring(87, 92));
         }
-        String quality = line.substring(92,93);
-        if(airTemperature != MISSING && quality.matches("[01459]")){
-            context.write(new Text(year),new IntWritable(airTemperature));
+        String quality = line.substring(92, 93);
+        if (airTemperature != MISSING && quality.matches("[01459]")) {
+            if (airTemperature > 1000) {
+                System.err.println("Temperature over 100 degrees for input: " + value);
+                context.setStatus("Detected possibly corrupt record: see logs.");
+                context.getCounter(Temperature.OVER_100).increment(1);
+            }
+            context.write(new Text(year), new IntWritable(airTemperature));
         }
     }
 }
